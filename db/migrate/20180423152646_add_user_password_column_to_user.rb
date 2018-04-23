2cbd0dedc35d97df5bfbd5e7f5786be7862f02ed
alphonsesantoro <kslagnes@mimer.no>
2018-04-23 17:39:47 +0200

    no longer use docker, Windows users can use bash on ubuntu on windows

---
+++ db/migrate/20180423152646_add_user_password_column_to_user.rb
@@ -0,0 +1,13 @@
+class AddUserPasswordColumnToUser < ActiveRecord::Migration[5.1]
+  def up
+    add_column :users, :username, :string
+    add_column :users, :password, :string
+    add_column :users, :salt, :string
+  end
+
+  def down
+    remove_column :users, :username
+    remove_column :users, :password
+    remove_column :users, :salt
+  end
+end
