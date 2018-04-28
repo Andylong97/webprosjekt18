d3a4a7557f8fb41709894699d6de25c01393000a
Mrnatox <olav.angell@gmail.com>
2018-04-28 11:41:06 +0200

    added subsites

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 # Home Controller
 class HomeController < ApplicationController
   def index
-    maps = ::GoogleMaps.new(params)
-    @fra = maps.origin
-    @til = maps.destination
+
   end
 
   def logistic_planner
