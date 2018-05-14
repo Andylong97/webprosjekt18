73ca17382149fa2f9c713409ea5e943e716748fb
alphonsesantoro <kslagnes@mimer.no>
2018-05-14 16:42:30 +0200

    precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-config.assets.precompile << proc { |path|
+Rails.application.config.assets.precompile << proc { |path|
   true if /\.(eot|svg|ttf|woff)\z/.match?(path)
 }
