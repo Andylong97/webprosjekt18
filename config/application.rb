628416b73c72916e7a828c1d1edb4d000530cba5
alphonsesantoro <kslagnes@mimer.no>
2018-05-14 17:21:49 +0200

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Webprosjekt18
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :nb
+    config.assets.precompile += %w[*.ttf *.woff *.svg *.eot]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
