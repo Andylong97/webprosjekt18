45202ed5836224e6fe587327fd268c6ada992c6b
alphonsesantoro <kslagnes@mimer.no>
2018-05-15 17:13:26 +0200

    precompile assets

--- app/classes/directions.rb
+++ app/classes/directions.rb
@@ -1,7 +1,7 @@
 class Directions
 
   def initialize(params, session)
-    locale = session[:locale] ? 'en' : 'no'
+    locale = session ? 'en' : 'no'
     url = URI("https://maps.googleapis.com/maps/api/directions/json?origin=#{params[:from]}&destination=#{params[:to]}&mode=transit&alternatives=true&language=#{locale}&key=#{ENV['GOOGLE_MAPS_API']}")
     puts url
     @direction = JSON.parse(Net::HTTP.get(url))
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Location < ApplicationRecord
+
+  def self.all_except(except)
+    list = []
+    Location.where('NOT address LIKE ?', except).each do |f|
+      list.push(name: f.name, address: f.address)
+    end
+    list
+  end
 end
